c6ea6fdc41b23ae65fda9447c5092605d538fa48
Chimpiri Rohith <19105008rohith@gmail.com>
2022-09-24 18:01:00 +0000

    finished navbar

--- portfolio/src/Components/Navbar/Navbar.tsx
+++ portfolio/src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 
 import styles from "./Navbar.module.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button/Button';
 
 // type NavbarProps = {
 //     name: string;
@@ -8,15 +10,35 @@ import styles from "./Navbar.module.css";
 // }
 
 function Navbar():ReactElement {
+
+  const [openNav, setOpenNav] = useState<boolean>(true);
+
+const handleClick = () => {
+  const el = document.querySelector(`.${styles.navItems}`) as HTMLElement;
+  if (openNav) {
+    el.style.top = "0px";
+  }
+  else{
+    el.style.top = "-250px";
+  }
+  setOpenNav(!openNav);
+}
+
+
   return (
     <div className={styles.navbar}>
         <div className={styles.navLogo}>C<span>R</span>R</div>
         <div className={styles.navItems}>
             <div className={styles.navItem}>Home</div>
-            <div className={styles.navItem}>Home</div>
-            <div className={styles.navItem}>Home</div>
-            <div className={styles.navItem}>Home</div>
+            <div className={styles.navItem}>About</div>
+            <div className={styles.navItem}>Skills</div>
+            <div className={styles.navItem}>Contact</div>
         </div>
+    <div className={styles.menu}>
+      <Button onClick={handleClick} color="inherit">
+        <MenuIcon />
+      </Button>
+    </div>
     </div>
   )
 }
